8837756036f768320b592bf92819ec44f54ed340
sribalajikrish <sribalajidit@gmail.com>
2026-09-12 19:05:06 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -56,24 +56,24 @@ export default defineConfig({
       // dependencies: ['setup'],
     },
 
-  //   {
-  //     name: 'firefox',
-  //     use: { 
-  //       ...devices['Desktop Firefox'],
-  //       storageState: '.auth/user.json',
-  //     },
-  //     // dependencies: ['setup'],
-  //   },
+    // {
+    //   name: 'firefox',
+    //   use: { 
+    //     ...devices['Desktop Firefox'],
+    //     storageState: '.auth/user.json',
+    //   },
+    //   // dependencies: ['setup'],
+    // },
 
-  //   {
-  //     name: 'webkit',
-  //     use: { 
-  //       ...devices['Desktop Safari'],
-  //       storageState: '.auth/user.json',
-  //     },
-  //     // dependencies: ['setup'],
-  //   },
-  // ],
+    // {
+    //   name: 'webkit',
+    //   use: { 
+    //     ...devices['Desktop Safari'],
+    //     storageState: '.auth/user.json',
+    //   },
+    //   // dependencies: ['setup'],
+    // },
+  ],
 
 
   /* Run your local dev server before starting the tests */
